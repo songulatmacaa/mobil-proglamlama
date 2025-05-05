b9f3faee18d900bd6bfde819c230037684a4ef53
Songül Atmaca <144557129+songulatmacaa@users.noreply.github.com>
2025-05-05 11:08:19 +0300

    Add files via upload

---
+++ ekrangecis/app/src/main/java/com/example/ekrangecis/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.ekrangecis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button buttonGeri = findViewById(R.id.button2);
+
+        buttonGeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
